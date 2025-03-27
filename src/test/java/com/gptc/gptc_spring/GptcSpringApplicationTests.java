99d326ed1e41dda40a333d204a22a114fd622c21
ndk <namdkgo@gmail.com>
2025-03-27 22:49:23 +0900

    환경구축

---
+++ src/test/java/com/gptc/gptc_spring/GptcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gptc.gptc_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptcSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
